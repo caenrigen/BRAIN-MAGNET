446ddaa2a7765fdd68c92819585d0741d92ae8f8
caenrigen <31376402+caenrigen@users.noreply.github.com>
2025-03-12 15:13:28 +0100

    Adapt training code

--- examples/training.py
+++ examples/training.py
@@ -14,6 +14,25 @@
 # ---
 
 # %%
+# %load_ext jupyter_black
+# %load_ext autotime
+
+# %%
+import numpy as np
+import seaborn as sns
+import scipy as sp
+import matplotlib.pyplot as plt
+from scipy import stats
+import pandas as pd
+import math
+import time
+import random
+from tqdm.auto import tqdm
+
+random_state = 913
+random.seed(random_state)
+
+# %%
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 from torch.utils.tensorboard import SummaryWriter
@@ -32,48 +51,35 @@ from torch.nn import (
 )
 from torchsummary import summary
 from torchmetrics import SpearmanCorrCoef, PearsonCorrCoef
-import numpy as np
-import seaborn as sns
-import scipy as sp
-import matplotlib.pyplot as plt
-from scipy import stats
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import math
 
-import random
+# %%
+torch.cuda.is_available(), torch.backends.mps.is_available()
 
-random.seed(913)
+# %%
+# device = torch.device("cuda")
+device = torch.device("cpu")
+# device = torch.device("mps") # for macOS >=14.0
+device
 
-import time
 
-import sys
-
-sys.path.append("/data/scratch/rdeng/enhancer_project/ipython_notebooks/")
-from helper import IOHelper, SequenceHelper, utils
-
-# np.set_printoptions(threshold=sys.maxsize)
-
-# training device: gpu or cpu
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("The device is {}".format(device))
+# %% [raw]
+# # import sys
+# # sys.path.append("/data/scratch/rdeng/enhancer_project/ipython_notebooks/")
+# from helper import IOHelper, SequenceHelper, utils
+# # np.set_printoptions(threshold=sys.maxsize)
 
 # %% [markdown]
 # ### Funtions to load data
 
 # %%
+def to_uint8(string):
+    return np.frombuffer(string.encode("ascii"), dtype=np.uint8)
+
+
 # function to convert only one piece of sequence to np.array
-from typing import Any, Callable, Dict, Optional, Text, Union, Iterable
-
-
-def one_hot_encode(
-    sequence: str,
-    alphabet: str = "ACGT",
-    neutral_alphabet: str = "N",
-    neutral_value: Any = 0,
-    dtype=np.float32,
-) -> np.ndarray:
+def make_one_hot_encode(alphabet: str = "ACGT", dtype=np.int8) -> np.ndarray:
     """
     One-hot encode for a sequence.
     A -> [1,0,0,0]
@@ -83,108 +89,107 @@ def one_hot_encode(
     N -> [0,0,0,0]
     """
 
-    def to_uint8(string):
-        return np.frombuffer(string.encode("ascii"), dtype=np.uint8)
-
     hash_table = np.zeros((np.iinfo(np.uint8).max, len(alphabet)), dtype=dtype)
     hash_table[to_uint8(alphabet)] = np.eye(len(alphabet), dtype=dtype)
-    hash_table[to_uint8(neutral_alphabet)] = neutral_value
-    hash_table = hash_table.astype(dtype)
-    return hash_table[to_uint8(sequence)]
+    return hash_table
 
 
+HASH_TABLE = make_one_hot_encode()
+
+
+def one_hot_encode(sequence: str):
+    return HASH_TABLE[to_uint8(sequence)]
+
+
+def pad_post_one_hot(snippet: np.ndarray, arr_pre: int = 1000):
+    assert len(snippet) <= arr_pre, len(snippet)
+    arr_len = len(snippet)
+    pad = (arr_pre - arr_len) / 2
+    return np.pad(snippet, [(int(pad), math.ceil(pad)), (0, 0)], mode="constant")
+
+
+seq = "ACGTATCnotdna"
+to_uint8(seq), one_hot_encode(seq)
+
+# %% [markdown]
+# ### Load sequences data
+
+# %%
+from pathlib import Path
+
+dbm = Path("/Volumes/Famafia/brain-magnet/")
+dbmrd = dbm / "rd_APP_data"
+dbmt = dbm / "train"
+
+# %%
+# Generated with get_seqs_from_hg38.R based on Enhancer_activity.txt
+dfe = pd.read_csv(dbmrd / "Enhancer_activity_w_seq_aug.txt.gz")
+dfe
+
+
+# %% [markdown]
+# ### Convert data to tensors
+
+# %%
 # function to generate all sequences to np.array (one-hot encoding matrix)
-def generate_sequence_matrix(input_fasta):
+def generate_sequence_matrix(sequences):
     """
     After the function of one_hot_encode,
-    convert the whole dateset to one_hot encoding matrix
+    convert the whole dataset to one_hot encoding matrix
     """
     sequence_matrix = []
-    for i in range(0, len(input_fasta.sequence)):
-        snippet = one_hot_encode(input_fasta.sequence[i])
-
-        arr_pre = 1000
-        arr_len = len(snippet)
-        arr_int = int((arr_pre - arr_len) / 2)
-        arr_ceil = math.ceil((arr_pre - arr_len) / 2)
-
-        snippet = np.pad(snippet, [(arr_int, arr_ceil), (0, 0)], mode="constant")
-        sequence_matrix.append(snippet)
-
-    sequence_matrix = np.array(sequence_matrix)
-    return sequence_matrix
+    for seq in sequences:
+        snippet = one_hot_encode(seq)
+        sequence_matrix.append(pad_post_one_hot(snippet))
+    return np.array(sequence_matrix)
 
 
-# convert np.array to tensor
-def prepare_tensor(X_test, Y_test):
+def prepare_tensor(X, Y):
     """
     convert np.array to tensor
     """
-    tensor_x = (
-        torch.Tensor(X_test).permute(0, 2, 1).unsqueeze(2)
-    )  # convert input: [batch, 1000, 4] -> [batch, 4, 1, 1000]
-    tensor_y = torch.Tensor(Y_test).unsqueeze(
-        1
-    )  # add one dimension in targets: [batch] -> [batch, 1]
-    tensor_data = TensorDataset(tensor_x, tensor_y)
-    return tensor_data
+    # convert input: [batch, 1000, 4] -> [batch, 4, 1, 1000]
+    tensor_x = torch.Tensor(X).permute(0, 2, 1).unsqueeze(2)
+    # add one dimension in targets: [batch] -> [batch, 1]
+    tensor_y = torch.Tensor(Y).unsqueeze(1)
+    return TensorDataset(tensor_x, tensor_y)
 
 
-# function to load sequences and enhancer activity
-def create_dataset(set, batch_size, task):
+def create_dataset(df: pd.DataFrame, batch_size: int, task: str):
     """
     Load sequences and enhancer activity,
     convert to tensor type as the input of model
     """
-    # Read fasta files
-    file_seq = str(
-        "/data/scratch/rdeng/enhancer_project/data/train_set/Sequences_" + set + ".fa"
-    )
-    input_fasta = IOHelper.get_fastas_from_file(file_seq, uppercase=True)
+    assert task in ("NSC", "ESC"), task
+    Y = df[f"{task}_log2_enrichment"].values
 
-    # Convert sequence to one hot encoding matrix
-    seq_matrix = generate_sequence_matrix(input_fasta)
+    # Convert each sequence to one-hot encoding
+    seq_matrix = generate_sequence_matrix(df.Seq.values)
 
-    X = np.nan_to_num(
-        seq_matrix
-    )  # Replace NaN with zero and infinity with large finite numbers
+    # Replace NaN with zero and infinity with large finite numbers
+    X = np.nan_to_num(seq_matrix)
     X_reshaped = X.reshape((X.shape[0], X.shape[1], X.shape[2]))
 
-    Activity = pd.read_table(
-        "/data/scratch/rdeng/enhancer_project/data/train_set/Sequences_activity_"
-        + set
-        + ".txt"
-    )
-
-    # choose task and create the target
-    if task == "NSC":
-        Y = Activity.NSC_log2_enrichment
-    elif task == "ESC":
-        Y = Activity.ESC_log2_enrichment
-    else:
-        print("Please provide the correct task ID: NSC or ESC")
-
     tensor_data = prepare_tensor(X_reshaped, Y)
-    tensor_size = len(tensor_data)
-    tensor_dataloader = DataLoader(tensor_data, batch_size)
-    print("the {} data size is {}".format(set, len(tensor_data)))
 
-    return tensor_dataloader
+    return DataLoader(tensor_data, batch_size)
 
 
 # %% [markdown]
 # ### Define EarlyStopping class
 
 # %%
-import numpy as np
-import torch
-
-
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
 
     def __init__(
-        self, patience=10, verbose=False, delta=0, trace_func=print, task="NSC"
+        self,
+        patience=20,
+        verbose=False,
+        delta=0,
+        trace_func=print,
+        task="NSC",
+        path=dbmt,
     ):
         """
         Args:
@@ -207,7 +212,7 @@ class EarlyStopping:
         self.val_loss_min = np.Inf
         self.delta = delta
         self.task = task
-        #         self.path = path
+        self.path = path
         self.trace_func = trace_func
 
     def __call__(self, val_loss, model):
@@ -218,11 +223,8 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model, task)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            self.trace_func(
-                "EarlyStopping counter: {} out of {}".format(
-                    self.counter, self.patience
-                )
-            )
+            msg = f"EarlyStopping {self.counter = } out of {self.patience = }"
+            self.trace_func(msg)
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -232,16 +234,11 @@ class EarlyStopping:
 
     def save_checkpoint(self, val_loss, model, task):
         """Saves model when validation loss decrease."""
+        fp = dbmt / f"checkpoint_{task}.pth"
         if self.verbose:
-            self.trace_func(
-                "Validation loss decreased ({:.5f} --> {:.5f}).  Saving model ...".format(
-                    self.val_loss_min, val_loss
-                )
-            )
-        torch.save(
-            model.state_dict(),
-            "/data/scratch/rdeng/enhancer_project/model/checkpoint_{}.pth".format(task),
-        )
+            msg = f"Validation loss decreased ({self.val_loss_min:.5f} --> {val_loss:.5f}). Saving model: {fp}"
+            self.trace_func(msg)
+        torch.save(model.state_dict(), fp)
         self.val_loss_min = val_loss
 
 
@@ -249,12 +246,10 @@ class EarlyStopping:
 # ### Build deep learning model
 
 # %%
-# Construct CNN model
-
-
 class CNN_STARR(nn.Module):
     """
-    CNN model: 4 convolution layers followed with two linear layers. forward two heads.
+    CNN model: 4 convolution layers followed with two linear layers.
+    forward two heads.
     """
 
     def __init__(self):
@@ -289,8 +284,7 @@ class CNN_STARR(nn.Module):
         )
 
     def forward(self, x):
-        x = self.model(x)
-        return x
+        return self.model(x)
 
 
 cnn_starr = CNN_STARR()
@@ -309,7 +303,7 @@ loss_fn = MSELoss()
 loss_fn.to(device)
 
 # optimizer
-learning_rate = 1e-2
+learning_rate = 0.01
 optimizer = torch.optim.Adam(cnn_starr.parameters(), lr=learning_rate)
 
 
@@ -318,7 +312,18 @@ optimizer = torch.optim.Adam(cnn_starr.parameters(), lr=learning_rate)
 
 
 # %%
-def train_model(model, batch_size=128, patience=10, epochs=100, task="NSC"):
+def train_model(
+    model,
+    loss_fn,
+    optimizer,
+    train_dataloader,
+    valid_dataloader,
+    batch_size=128,
+    patience=20,
+    epochs=100,
+    task="NSC",
+    device=device,
+):
     start_time = time.time()
     # to track the training loss as the model trains
     train_losses = []
@@ -332,9 +337,9 @@ def train_model(model, batch_size=128, patience=10, epochs=100, task="NSC"):
     # initialize the early_stopping_object
     early_stopping = EarlyStopping(patience, verbose=True)
 
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         #################
-        # Train the model#
+        # Train the model
         #################
 
         ### index: 0 is NSC, index: 1 is ESC ###
@@ -344,7 +349,7 @@ def train_model(model, batch_size=128, patience=10, epochs=100, task="NSC"):
             inputs = inputs.to(device)
             targets = targets.to(device)
             outputs = model(inputs)
-            # loss function
+
             loss = loss_fn(outputs, targets)
             # record training loss
             train_losses.append(loss.item())
@@ -355,7 +360,7 @@ def train_model(model, batch_size=128, patience=10, epochs=100, task="NSC"):
             optimizer.step()
 
         ####################
-        # Validate the model#
+        # Validate the model
         ####################
         model.eval()  # pre model for evaluation
         with torch.no_grad():
@@ -374,37 +379,28 @@ def train_model(model, batch_size=128, patience=10, epochs=100, task="NSC"):
         avg_train_losses.append(train_loss)
         avg_valid_losses.append(valid_loss)
 
-        print(
-            "[ {} / {} ], train_loss: {:.5f}, valid_loss {:.5f}".format(
-                epoch + 1, epochs, train_loss, valid_loss
-            )
-        )
-
         end_time = time.time()
-        duration_time = end_time - start_time
-        print("---------training time: {:.2f}s---------".format(duration_time))
+        dt = end_time - start_time
+
+        msg = f"[{epoch + 1:2d}/{epochs}| {dt=:4.2f}s], train_loss: {train_loss:.5f}, valid_loss {valid_loss:.5f}"
+        print(msg)
 
         # clear lists to track next epoch
         train_losses = []
         valid_losses = []
 
         # early_stopping needs the validation loss to check if it has decrease
-        # and if it hasm it will make a checkpoint of the current model
-        early_stopping(
-            valid_loss,
-            model,
-        )
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(valid_loss, model)
 
         if early_stopping.early_stop:
             print("Early stopping")
             break
 
     # load the last checkpoint with the best model
-    model.load_state_dict(
-        torch.load(
-            "/data/scratch/rdeng/enhancer_project/model/checkpoint_{}.pth".format(task)
-        )
-    )
+    fp = dbmt / f"checkpoint_{task}.pth"
+    model.load_state_dict(torch.load(fp))
+
     return model, avg_train_losses, avg_valid_losses
 
 
@@ -431,11 +427,6 @@ def loss_fig(train_loss, valid_loss, task):
     plt.legend()
     plt.tight_layout()
     plt.show()
-    fig.savefig(
-        "/data/scratch/rdeng/enhancer_project/plots/loss_{}.pdf".format(task),
-        dpi=300,
-        bbox_inches="tight",
-    )
 
 
 # %% [markdown]
@@ -444,7 +435,7 @@ def loss_fig(train_loss, valid_loss, task):
 
 
 # %%
-def summmary_statistic(set, dataloader, main_model, task):
+def summmary_statistic(set_name, dataloader, main_model, task):
     # initialize loss
     test_loss = 0.0
     whole_preds = []
@@ -476,26 +467,29 @@ def summmary_statistic(set, dataloader, main_model, task):
 
     # summary data for NSC
 
-    print("---------Summary on {} data---------".format(set))
+    print("---------Summary on {} data---------".format(set_name))
     print("the MSE of {}: {:.5f}".format(task, MSE))
     print("the pearson r of {}: {:.5f}".format(task, pearon[0]))
     print("the spearman r of {}: {:.5f}".format(task, spearman[0]))
 
     # plot correlation
     df = pd.DataFrame({"targets": whole_targets, "preds": whole_preds.squeeze(1)})
+    return df
 
-    utils.density_scatter(
-        x=df.targets,
-        y=df.preds,
-        bins=[1500, 1500],
-        dot_size=5,
-        color_map="turbo",
-        save="/data/scratch/rdeng/enhancer_project/plots/{} correlation on {} data.pdf".format(
-            task, set
-        ),
-        title="The correlation of {} data".format(set),
-    )
 
+# %% [markdown]
+# ### Split data from training
+
+# %%
+# Small sample for testing code
+df = dfe.sample(n=4_000, random_state=random_state)
+
+df_train, df_tmp = train_test_split(df, test_size=0.10, random_state=random_state)
+df_val, df_test = train_test_split(df_tmp, test_size=0.50, random_state=random_state)
+del df_tmp
+
+# %%
+df_train
 
 # %% [markdown]
 # ### Run the pipline
@@ -503,22 +497,29 @@ def summmary_statistic(set, dataloader, main_model, task):
 # %%
 batch_size = 128
 epochs = 100
-patience = 20
+patience = 10
 
-# define the task
-global task
 task = "NSC"
 
 # create data
-train_dataloader = create_dataset("Train", batch_size, task)
-valid_dataloader = create_dataset("Valid", batch_size, task)
-test_dataloader = create_dataset("Test", batch_size, task)
+train_dataloader = create_dataset(df_train, batch_size, task)
+valid_dataloader = create_dataset(df_val, batch_size, task)
+test_dataloader = create_dataset(df_test, batch_size, task)
 
 # train model
 cnn_starr, train_loss, valid_loss = train_model(
-    cnn_starr, batch_size, patience, epochs, task
+    model=cnn_starr,
+    loss_fn=loss_fn,
+    optimizer=optimizer,
+    train_dataloader=train_dataloader,
+    valid_dataloader=valid_dataloader,
+    batch_size=batch_size,
+    patience=patience,
+    epochs=epochs,
+    task=task,
 )
 
+# %%
 # plot Train and Valid loss
 loss_fig(train_loss, valid_loss, task)
 
