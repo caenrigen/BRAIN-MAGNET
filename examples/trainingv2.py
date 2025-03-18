1a01091ec67995330acdd32de8750b12256619d9
caenrigen <31376402+caenrigen@users.noreply.github.com>
2025-03-18 14:00:48 +0100

    Add notebook

--- examples/trainingv2.py
+++ examples/trainingv2.py
@@ -30,8 +30,20 @@ import time
 import random
 from tqdm.auto import tqdm
 from pathlib import Path
-import gc
+from typing import Optional
+from functools import partial
 
+# %%
+import lightning as L
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from torch.utils.tensorboard import SummaryWriter
+
+# %%
 random_state = 913
 random.seed(random_state)
 
@@ -40,5 +52,47 @@ dbmrd = dbm / "rd_APP_data"
 dbmt = dbm / "train"
 
 # %%
-# confirm tqdm is working well
-tqdm(range(1000))
+print(torch.cuda.is_available(), torch.backends.mps.is_available())
+# device = torch.device("cuda")
+# device = torch.device("cpu")
+device = torch.device("mps")  # might have priblems for macOS <14.0
+device
+
+# %%
+# Evaluate the python files within the notebook namespace
+# %run -i auxiliar.py
+# %run -i cnn_starr.py
+# %run -i data_module.py
+
+task = "ESC"
+
+model = CNNSTARR(lr=0.005, weight_decay=0, revcomp=True, log_vars_prefix=task)
+model.to(device)
+
+fp = dbmrd / "Enhancer_activity_w_seq.csv.gz"
+data_loader = DMSTARR(fp=fp, sample=None, y_col=f"{task}_log2_enrichment")
+
+early_stop = EarlyStopping(
+    monitor=f"{task}_loss_val",
+    min_delta=0.001,
+    patience=20,
+    verbose=True,
+    mode="min",
+)
+
+logger = TensorBoardLogger(dbmt, name=f"starr_{task}")
+trainer = L.Trainer(
+    max_epochs=100,
+    logger=logger,
+    callbacks=[early_stop],
+)
+
+trainer.fit(model, data_loader)  # run training
+
+# %%
+trainer.logger
+
+# %%
+# ?torch.optim.Adam
+
+# %%
