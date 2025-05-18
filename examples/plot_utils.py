ae82c35c60c1fc14af2f45bd3bf667defa047620
caenrigen <31376402+caenrigen@users.noreply.github.com>
2025-05-18 19:56:32 +0200

    Rename module

--- examples/plot_utils.py
+++ examples/plot_utils.py
@@ -1,5 +1,7 @@
+from typing import Optional
 import numpy as np
 from scipy.interpolate import interpn
+from deeplift.visualization import viz_sequence
 
 
 def density_scatter(x, y, ax, sort=True, bins=100, cmap="magma", **kwargs):
