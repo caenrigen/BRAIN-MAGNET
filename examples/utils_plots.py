1a9b3077f4ba14af0d67b4f09ea15715695f1158
caenrigen <31376402+caenrigen@users.noreply.github.com>
2025-05-18 13:26:58 +0200

    Clean up

--- examples/utils_plots.py
+++ examples/utils_plots.py
@@ -1,12 +1,13 @@
 import numpy as np
 from scipy.interpolate import interpn
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# from matplotlib.colors import Normalize
 
 
-def density_scatter(x, y, ax, fig, sort=True, bins=100, cmap="magma", **kwargs):
-    """Scatter plot colored by 2d histogram"""
+def density_scatter(x, y, ax, sort=True, bins=100, cmap="magma", **kwargs):
+    """
+    Scatter plot colored by 2d histogram
+
+    Source: https://stackoverflow.com/a/53865762/9047715
+    """
     data, x_e, y_e = np.histogram2d(x, y, bins=bins, density=True)
     z = interpn(
         (0.5 * (x_e[1:] + x_e[:-1]), 0.5 * (y_e[1:] + y_e[:-1])),
@@ -19,15 +20,9 @@ def density_scatter(x, y, ax, fig, sort=True, bins=100, cmap="magma", **kwargs):
     # To be sure to plot all data
     z[np.where(np.isnan(z))] = 0.0
 
-    # Sort the points by density, so that the densest points are plotted last
+    # Sort the points by density, so that the densest points are plotted last (on top)
     if sort:
         idx = z.argsort()
         x, y, z = x[idx], y[idx], z[idx]
 
-    ax.scatter(x, y, c=z, **kwargs)
-
-    # norm = Normalize(vmin=np.min(z), vmax=np.max(z))
-    # cbar = fig.colorbar(cm.ScalarMappable(norm=norm), ax=ax)
-    # cbar.ax.set_ylabel("Density")
-
-    return ax
+    ax.scatter(x, y, c=z, cmap=cmap, **kwargs)
